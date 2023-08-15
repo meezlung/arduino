fa83ebfc3fea62e5579ac7bf2be442e0012f46a7
gabr_m <65329581+meezlung@users.noreply.github.com>
2023-08-15 22:12:37 +0800

    Add files via upload

---
+++ research/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "SKYfiberCDAB"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "512834439"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2056534			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "6WYJ4R17KGA7YC5D"   // replace XYZ with your channel write API Key
